fb6b9e6301f0db21d0b2700ff5b13b6684293b23
jiunnyuan1991 <33826590+jiunnyuan1991@users.noreply.github.com>
2020-04-07 15:35:01 +0800

    new build

---
+++ Monitoring in a Box/WebGUI/frontend/build/precache-manifest.76e74757adabf12031119b8c4f5ea3d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf43b1f551c769a8efa90dafa4674b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4037cffe20695634ffc8",
+    "url": "/static/css/main.2a1fc253.chunk.css"
+  },
+  {
+    "revision": "bff42af4afc873a67335",
+    "url": "/static/js/2.9c97a09d.chunk.js"
+  },
+  {
+    "revision": "0820db36c89eca99a13727075ed20644",
+    "url": "/static/js/2.9c97a09d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4037cffe20695634ffc8",
+    "url": "/static/js/main.ded76a4d.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "b3c0d98bf6676e63a7bc9b8712cf33da",
+    "url": "/static/media/coverphoto.b3c0d98b.jpg"
+  },
+  {
+    "revision": "33d46e7cf3159d286d3129c9b5feab74",
+    "url": "/static/media/favicon.33d46e7c.ico"
+  }
+]);
